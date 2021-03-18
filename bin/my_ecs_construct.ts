00bbbee68320fb9f876e26bd7504e11fa1083a88
adi <adi.ofry@verbit.ai>
2021-03-18 22:23:57 +0200

    init

--- bin/my_ecs_construct.ts
+++ bin/my_ecs_construct.ts
@@ -3,5 +3,10 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { MyEcsConstructStack } from '../lib/my_ecs_construct-stack';
 
+const envEU = {
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: 'eu-central-1'
+}
+
 const app = new cdk.App();
-new MyEcsConstructStack(app, 'MyEcsConstructStack');
+new MyEcsConstructStack(app, 'MyEcsConstructStack', { env: envEU });
